073334c4b8031145939d4553b1a8109f503ae706
Torontow <mtorontow0@gmail.com>
2020-11-30 16:20:28 -0500

    Adding packages and template
    Had to uninstall inquirer so I could initialize first. I did that, and built a README template for the generator function.

--- index.js
+++ index.js
@@ -1,6 +1,50 @@
-const fs = require('fs');
 const inquirer = require('inquirer');
+const fs = require('fs');
 const util = require('util');
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
+
+
+
+
+
+
+const generateReadme = (answers) =>
+`# ${} PROJECT TITLE
+
+## TABLE OF CONTENTS
+
+1. [Description](#Description)
+2. [Installation]
+3. [Usage]
+4. [Contributing]
+5. [Tests]
+6. [Questions/Contact]
+
+## Description
+
+${}
+
+## Installation
+
+${}
+
+## Usage
+
+${}
+
+## Contributing
+
+${} 
+
+## Tests
+
+${}
+
+## Questions/Contact
+
+[GitHub Profile: http://github.com/${}] (http://github.com/${})
+[Email: ${}]
+
+`
