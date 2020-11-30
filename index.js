31395ec440df6ecccef3909a17b1d0877f7be976
Torontow <mtorontow0@gmail.com>
2020-11-30 16:49:07 -0500

    Inquirer Questions Added
    Built the questionnaire portion of the code.

--- index.js
+++ index.js
@@ -4,14 +4,48 @@ const util = require('util');
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
-
-
-
-
+const promptUser = () =>
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of your project.',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'How do users install your project for their use?',
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'List contributors to your project.',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Describe the testing used for your project.',
+        },
+        {
+            type: 'input',
+            name: 'gitHubName',
+            message: 'What is your GitHub user name?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your e-mail address?'
+        },
+    ]);
 
 
 const generateReadme = (answers) =>
-`# ${} PROJECT TITLE
+    `# ${title} PROJECT TITLE
 
 ## TABLE OF CONTENTS
 
@@ -47,4 +81,4 @@ ${}
 [GitHub Profile: http://github.com/${}] (http://github.com/${})
 [Email: ${}]
 
-`
+`;
