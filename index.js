bef909bc61dac6f6e324d8f95e06f5fc4c79a2ec
Torontow <mtorontow0@gmail.com>
2020-12-01 13:07:02 -0500

    License Section Added
    User input from this section will display as a badge below the project title, and include a reference to said license in a License section of the README.

--- index.js
+++ index.js
@@ -7,49 +7,64 @@ const writeFileAsync = util.promisify(fs.writeFile);
 const promptUser = () =>
     inquirer.prompt([
         {
-            type: 'input',
             name: 'title',
             message: 'What is the title of your project?',
         },
         {
-            type: 'input',
             name: 'description',
             message: 'Provide a description of your project.',
         },
         {
-            type: 'input',
             name: 'installation',
             message: 'How do users install your project for their use?',
         },
         {
-            type: 'input',
             name: 'usage',
             message: 'Describe how to use your project.',
         },
         {
-            type: 'input',
             name: 'contributing',
             message: 'List contributors to your project.',
         },
         {
-            type: 'input',
             name: 'tests',
             message: 'Describe the testing used for your project.',
         },
         {
-            type: 'input',
             name: 'gitHubName',
             message: 'What is your GitHub user name?',
         },
         {
-            type: 'input',
             name: 'email',
             message: 'What is your e-mail address?'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Please choose a license for your project.',
+            default: 'MIT',
+            choices: [
+                {
+                    name: 'ISC',
+                    value: ['ISC','[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)'],
+                },
+                {
+                    name: 'GNU',
+                    value: ['GNU','[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'],
+
+                },
+                {
+                    name: 'MIT',
+                    value: ['MIT','[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'],
+                },
+            ]
         },
     ]);
 
 const generateReadme = (answers) =>
 `# ${answers.title}
+
+${answers.license[1]}
 
 ## TABLE OF CONTENTS
 
@@ -59,6 +74,7 @@ const generateReadme = (answers) =>
 4. [Contributing](#Contributing)
 5. [Tests](#Tests)
 6. [Questions/Contact](#Questions/Contact)
+7. [License](#License)
 
 ## Description
 
@@ -84,7 +100,12 @@ ${answers.tests}
 
 GitHub Profile: [https://github.com/${answers.gitHubName}](https://github.com/${answers.gitHubName})
 Email: [${answers.email}](${answers.email})
+
+## License
+
+This project is covered under the ${answers.license[0]} license.
 `;
+
 
 promptUser()
     .then((answers) => writeFileAsync('README.md', generateReadme(answers)))
